74ce931d526f99ab31be4d46e3f3f73fa13c8fc5
Roman <igoshinr.estho@gmail.com>
2023-10-26 02:30:54 +0300

    com2

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
-/* 1) Дан массив */ const arr = [1, 5, 7, 9] /* с помощью Math.min и spread оператора,
-   найти минимальное число в массиве, решение задание должно состоять из одной
-   строки кода.*/
+/*Задание 1: "Управление библиотекой книг"
 
-console.log(Math.min(...arr));
+Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
 
-/* 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект
-   с двумя методами: increment и decrement. Метод increment должен увеличивать
-   значение счетчика на 1, а метод decrement должен уменьшать значение счетчика
-   на 1. Значение счетчика должно быть доступно только через методы объекта,
-   а не напрямую.*/
+Свойство title (название) - строка, название книги.
+Свойство author (автор) - строка, имя автора книги.
+Свойство pages (количество страниц) - число, количество страниц в книге.
+Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
 
-const createCounter = () => {
-    let count = 0
-    
-    function counter() {
-        return count;
+// Пример использования класса
+const book = new Book("BookName1", "Author1", 123);
+book.displayInfo(); // "Title: BookName1, Author: Author1, Pages: 123"*/
+
+class Book {
+    title;
+    author = '';
+    pages = 0;
+
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
     }
 
-    counter.increment = () => ++count;
-    counter.decrement = () => --count;
-
-    return counter;
+    displayInfo() {
+        console.log(`Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}`);
+    }
 }
 
-const counter1 = createCounter();
-console.log(counter1());
+const book1 = new Book("Тошнота", "Жан-Поль Сартр", 320);
 
+book1.displayInfo();
+
+/*Задание 2: "Управление списком студентов"
+Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
+
+Свойство name (имя) - строка, имя студента.
+Свойство age (возраст) - число, возраст студента.
+Свойство grade (класс) - строка, класс, в котором учится студент.
+Метод displayInfo() - выводит информацию о студенте в консоль.
+
+// Пример использования класса
+const student = new Student("John Smith", 16, "10th grade");
+student.displayInfo(); // "Name: John Smith, Age: 16, Grade: 10th grade"*/
+
+class Student {
+    name;
+    age;
+    grade;
+
+    constructor(name, age, grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    displayInfo() {
+        console.log(`Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`);
+    }
+}
+
+const me = new Student('Roman', 21, '1-st grade');
+
+me.displayInfo();
